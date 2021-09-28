43b7a26548fb5016d4f9316d18346d7f4e85d33d
Malika <shopanovamalika@mail.ru>
2021-09-29 01:43:36 +0600

    2.15. Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const getRndInteger = (min, max) => {
+  if (min >= 0 && max >= 0 && max >= min) {
+    return Math.round((Math.random() *(max-min + 1)) + min);
+  }
+  return 'Введите корректные значения';
+};
+
+getRndInteger(2, 6);
+
+const getRndDecimal = (min, max, decimal) => {
+  if (min >= 0 && max >= 0 && max >= min && decimal >= 0) {
+    return (Math.random() *(max-min + 1) + min).toFixed(decimal);
+  }
+  return 'Введите корректные значения';
+};
+
+getRndDecimal(2.5, 5.7, 3);
+
+
